8279ddea1f3ac4f9f52ebeec6cccc6c3d753e205
sreeja o s <sreejaosreenivasan@gmail.com>
2024-08-15 14:15:43 +0530

    scroll avoided

--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import "./Footer.css";
-import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-links">
-        {/* <a href="#home">Home</a>
-        <a href="#about">About</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact</a> */}
-        <Link to={"/"}>Home</Link>
-        <Link to={"/countries"}>Countries</Link>
-        <Link to={"/todo"}>ToDo</Link>
-
-      </div>
+   
       <div className="copyright">
         &copy; {new Date().getFullYear()} Your Company. All rights reserved.
       </div>
